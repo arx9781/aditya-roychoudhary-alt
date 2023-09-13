2663541e5fa690655c1b6d3e6209777b9d1320ce
kujojotaroooo <107541824+adityaRoychoudhary-exe@users.noreply.github.com>
2023-09-13 22:15:18 +0530

    added progress bar + tweaks

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,13 @@
-const toggleButton = document.getElementsByClassName("toggle");
-const body = document.body;
-
-toggleButton =
-  ("click",
-  () => {
-    document.body.style.background = "white";
+function updateProgressBar() {
+  const { scrollTop, scrollHeight } = document.documentElement;
+  const scrollPercent = `${
+    (scrollTop / (scrollHeight - window.innerHeight)) * 100
+  }%`;
+  const progressBar = document.querySelector(".progress-bar");
+  progressBar.style.setProperty("--progress", scrollPercent);
+  progressBar.style.setProperty("width", scrollPercent + "%", {
+    transition: "width 2s ease",
   });
+}
+
+document.addEventListener("scroll", updateProgressBar);
